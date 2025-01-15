c784c81d7a5b0b4d100be4c7ad295acaccf74766
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2025-01-15 18:13:06 +0200

    refactor main.py to streamline KD-tree operations and enhance range search functionality

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'kd'))
-
+import pandas as pd
 from kd import *
 
-
-import pandas as pd
 
 file = "Data/simplified_coffee.csv"
 
 df = pd.read_csv(file)
 
 # Convert 'review_date' to datetime format
-# df['review_date_comp'] = pd.to_datetime(df['review_date'], format='%B %Y').dt.strftime('%Y%m').astype(int)
+df['review_date_comp'] = pd.to_datetime(df['review_date'], format='%B %Y').dt.strftime('%Y%m').astype(int)
 
-# Convert the date to a timestamp (number of seconds since epoch)
-# df['review_date_comp'] = pd.to_datetime(df['review_date']).apply(lambda x: int(x.timestamp()))
-
-# One-hot encode the 'country' column
-# df = pd.get_dummies(df, columns=['origin'], drop_first=True)
-
-
-columns_to_index = ['rating', '100g_USD', 'review_date']
+columns_to_index = ['review_date_comp', 'rating', '100g_USD']
 
 # Build the KDTree
 root = build_kdtree(df, columns_to_index)
 
-# print_kdtree(root)
-
+# Validate the KDTree
 print(validate_kdtree(root, columns_to_index))
+
+# Define the range for the search
+min_range = [201901]
+max_range = [202112]
+
+# Perform the range search
+results = range_search(root, 0, columns_to_index, min_range, max_range)
+
+# Check if results are empty
+if not results:
+    print("No results found.")
+else:
+    # Convert results to a DataFrame for cleaner output
+    results_df = pd.DataFrame(results)
+
+    # Print the specified columns from the results
+    print(results_df[['review_date', 'rating', '100g_USD']])
